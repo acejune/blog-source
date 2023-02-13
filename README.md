f6131d310c3d1adf41c93ac74cac9691e6d745a2
acejune <junehanchoi@gmail.com>
2023-02-13 17:26:38 +0900

    First commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Hugo Blog 
+
+<br>
+
+### 글 쓰기
+
+```sh
+hugo new posts/{directory name}/index.md
+```
+
+<br>
+
+### 배포하기
+
+```sh
+./deploy.sh "{Commit message}"
+```
---
+++ deploy.sh
@@ -0,0 +1,24 @@
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+hugo -t DoIt
+
+cd public
+git add .
+
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+git push origin main
+
+
+cd ..
+git add .
+
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+git push origin main
